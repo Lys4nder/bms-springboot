0fa52873ab4ff96fcf490c3120e023fcc91d4d53
Lysander Pitu <lysanderpitu@yahoo.com>
2023-06-07 19:05:33 +0300

    added login logic

---
+++ src/main/java/com/project/LoginController.java
@@ -0,0 +1,36 @@
+package com.project;
+
+import com.project.user.User;
+import com.project.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @RequestMapping("/login")
+    public String showLoginPage() {
+        return "index";
+    }
+
+    @PostMapping("/login")
+    public String processLogin(String email, String password, Model model) {
+        User user = userRepository.findByEmail(email);
+
+        if (user != null && user.getPassword().equals(password)) {
+            model.addAttribute("message", "Login successful!");
+            return "redirect:/users/transactions/" + user.getId();
+        } else {
+            model.addAttribute("message", "Invalid email or password");
+            return "index";
+        }
+    }
+
+}
